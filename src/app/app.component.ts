fc5a4872f55de8461a2260d706fffb3f2b7516dd
huyenpt21 <huyenpthe140045@fpt.edu.vn>
2021-10-16 11:59:16 +0700

    upcode

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'HelloNgIf';
+  title = 'NgIf';
+
+  message = 'I\'m read only!';
+
+  canEdit = false;
+
+  fontColor = 'red';
+
+  onEditClick(){
+    this.canEdit = !this.canEdit;
+    if(this.canEdit){
+      this.message = "You can edit me!"; 
+      this.fontColor = 'green';
+    }else{
+      this.message = "I'm read only!";
+      this.fontColor = 'red';
+    }
+  }
 }
